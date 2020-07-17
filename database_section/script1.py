1da3201f4927429198f20f36449eb0a271aad63d
Szczepanski-Marcin <szmar@vp.pl>
2020-07-17 11:56:35 +0200

    Created new database in pgAdmin

--- database_section/script1.py
+++ database_section/script1.py
@@ -1,37 +1,44 @@
-import sqlite3
+import psycopg2
 
 def create_table():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("dbname='database 1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTIGER, price REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item, quantity, price):
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("lite.db")
     cur=conn.cursor()
     cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
     conn.commit()
     conn.close()
 
-insert("Cofee cup", 12,9)
 
 def view():
-    conn=sqlite3.connect("lite.db")
+    conn=psycopg2.connect("lite.db")
     cur=conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
     conn.close()
     return rows
 
-def view(item):
-    conn=sqlite3.connect("lite.db")
+def delete(item):
+    conn=psycopg2.connect("lite.db")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item))
+    cur.execute("DELETE FROM store WHERE item=?",(item,))
     conn.commit()
     conn.close()
 
+def update(quantity, price, item):
+    conn=psycopg2.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity, price, item))
+    conn.commit()
+    conn.close()
 
-delete("Wine Glass")
-
-print(view())
+create_table()
+#update(11,6,"Water")
+#delete("Wine glass")
+#print(view())
+#insert("Coffee Cup",10,5)
