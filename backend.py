fa62c91309d56ad16725bc83ee91e73a96db481c
Szczepanski-Marcin <szmar@vp.pl>
2020-07-24 20:59:57 +0200

    Updated the code

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+
 class Database:
+
 
     def __init__(self, db):
         self.conn = sqlite3.connect("books.db")
@@ -8,14 +10,17 @@ class Database:
         self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, ISBN integer)")
         self.conn.commit()
 
+
     def insert(self, title, author, year, ISBN):
         self.cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, ISBN))
         self.conn.commit()
+
 
     def view(self):
         self.cur.execute("SELECT * FROM book")
         rows=self.cur.fetchall()
         return rows
+
 
     def search(self, title="", author="", year="", ISBN=""):
         self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR ISBN=? ", (title, author, year, ISBN))
@@ -23,16 +28,20 @@ class Database:
         self.conn.commit()
         return rows
 
+
     def delete(self, id):
         self.cur.execute("DELETE FROM book WHERE id=?",(id,))
         self.conn.commit()
+
 
     def update(self, id,title, author, year, ISBN):
         self.cur.execute("UPDATE book SET title=?, author=?, year=?, ISBN=? WHERE id=?",(title, author, year, ISBN, id))
         self.conn.commit()
 
+
     def __del__(self):
         self.conn.close()
+
 
 #insert("The ssun", "John Smith", 1918, 91312121)
 #delete(3)
